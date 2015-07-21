69cc02db31ed1467f53f99d43e4b0818f59fefa9
Carlos <cfigueroa@gmail.com>
2015-07-21 21:55:27 +0200

    Añadimos BBDD SQLite

---
+++ models/models.js
@@ -0,0 +1,24 @@
+var path=require('path');
+
+//Cargar modelo ORM
+var Sequelize=require('sequelize');
+
+//Usar BBDD SQLite
+var sequelize = new Sequelize(null, null, null,
+			{dialect:"sqlite", storage:"quiz.sqlite"}
+			);
+
+//Importar la definición de la tabla Quiz en quiz.js
+var Quiz=sequelize.import(path.join(__dirname,'quiz'));
+
+exports.Quiz=Quiz;
+
+sequelize.sync().success(function(){
+  Quiz.count().success(function(count){
+    if (count===0){
+	Quiz.create({pregunta:'Capital de Italia',
+		     respuesta:'Roma'})
+	.success(function(){console.log('Base de datos inicializada')});
+	};
+  });
+});
