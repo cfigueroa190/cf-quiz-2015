bd7fe562f79957c6e453550c112bc15e620ec39c
Carlos <cfigueroa@gmail.com>
2015-07-22 20:00:37 +0200

    Lista de preguntas

--- models/models.js
+++ models/models.js
@@ -28,12 +28,14 @@ var Quiz=sequelize.import(path.join(__dirname,'quiz'));
 
 exports.Quiz=Quiz;
 
-sequelize.sync().success(function(){
-  Quiz.count().success(function(count){
+sequelize.sync().then(function(){
+  Quiz.count().then(function(count){
     if (count===0){
 	Quiz.create({pregunta:'Capital de Italia',
-		     respuesta:'Roma'})
-	.success(function(){console.log('Base de datos inicializada')});
+		     respuesta:'Roma'});
+	Quiz.create({pregunta:'Capital de Portugal',
+		     respuesta:'Lisboa'})
+	.then(function(){console.log('Base de datos inicializada')});
 	};
   });
 });
