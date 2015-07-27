64d1f8361768b952c9479cbdc6780a933fed6245
Carlos <cfigueroa@gmail.com>
2015-07-27 14:00:06 +0200

    Borrar preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -113,6 +113,15 @@ exports.update = function (req,res){
 };
 
 
+// DELETE /quizes/:id
+
+exports.destroy = function (req,res){
+	req.quiz.destroy().then(function(){
+	  res.redirect('/quizes');
+	}).catch(function(error){next(error)});
+
+};
+
 
 // GET /author
 
